4cf7519f6fb72d56ed69ce388964823d4268ed41
lawrenceagles <lawrenceagles@gmail.com>
2021-10-18 09:30:49 +0100

    TW boilerplate ready

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import { Fragment, useEffect } from 'react';
 import { selectIsConnectedToRoom, useHMSActions, useHMSStore } from '@100mslive/hms-video-react';
-import JoinForm from './components/JoinRoom';
-import Room from './components/Room';
-import Footer from './components/Footer';
 
-export default function App() {
-	const isConnected = useHMSStore(selectIsConnectedToRoom);
-	const hmsActions = useHMSActions();
+const App = () => {
+	return <div>Hello from App.js</div>;
+};
 
-	useEffect(
-		() => {
-			window.onunload = () => {
-				if (isConnected) {
-					hmsActions.leave();
-				}
-			};
-		},
-		[ hmsActions, isConnected ]
-	);
-
-	return (
-		<div className="App">
-			{isConnected ? (
-				<Fragment>
-					<Room />
-					<Footer />
-				</Fragment>
-			) : (
-				<JoinForm />
-			)}
-		</div>
-	);
-}
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HMSRoomProvider } from '@100mslive/hms-video-react';
-
 ReactDOM.render(
 	<React.StrictMode>
-		<HMSRoomProvider>
-			<App />
-		</HMSRoomProvider>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
